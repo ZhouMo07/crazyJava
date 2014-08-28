0b9236370f8759bb630f572e095f2df174ddb223
ZhouMo07 <zhoumo_07@163.com>
2014-08-28 18:31:05 +0800

    constructoe wit Eng remark

--- Wolf.java
+++ Wolf.java
@@ -4,23 +4,23 @@ class Creature
 {
 	public Creature()
 	{
-		System.out.println("Creature无参数的构造器");
+		System.out.println("Creature no parameter");
 	}
 }
 class Animal extends Creature
 {
 	public Animal()
 	{
-		System.out.println("Animal无参数的构造器");
+		System.out.println("Animal no parameter");
 	}
 	public Animal(String name)
 	{
-		System.out.println("Animal带一个参数的构造器，" + "该动物的名字为" + name);
+		System.out.println("Animal with a parameter，" + "the animal's name is: " + name);
 	}
 	public Animal(String name, int age)
 	{
 		this(name);
-		System.out.println("Animal带两个参数的构造器，" + "它的年龄为" + age);
+		System.out.println("Animal with two parameter，" + "its age is: " + age);
 	}
 }
 public class Wolf extends Animal
@@ -28,8 +28,8 @@ public class Wolf extends Animal
 	public Wolf()
 	{
 
-		super("灰太狼",3);
-		System.out.println("Wolf无参数的构造器");
+		super("wolf",3);
+		System.out.println("Wolf with no parameter");
 	}
 	public static void main(String[] args)
 	{
